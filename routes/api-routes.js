a1838c129a3f9dc297450fc36a3967fe31aa5cb3
Cole Rudolph <crudolph@gmail.com>
2019-10-11 17:32:05 -0400

    adding API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,13 +3,19 @@ const cheerio = require("cheerio");
 const db = require("../models");
 
 module.exports = function (app) {
-    app.get("/articles", function(req, res) {
-        request("https://www.apnews.com/", function(error, response, html) {
-            const $ = cheerio.load(html);
+    app.get("/scrape", function (req, res) {
+        axios.get("https://www.https://www.apnews.com/")
+            .then(function (response) {
+                const $ = cheerio.load(response.data)
 
-            let results = []
+                let results = {};
+                
+                let headline = $(element).find("a").find("h1").text();
+                let link = "https://apnews.com" + $(element).find("a").attr("href");
+                let image = $(element).find
 
-            console.log(results)
-        })
+            })
+
+        console.log(results)
     })
 }
