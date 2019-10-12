9f41c7c6a6586c959eccf0ec9a893869218995f3
Cole Rudolph <crudolph@gmail.com>
2019-10-12 01:56:57 -0400

    finished api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,19 +3,73 @@ const cheerio = require("cheerio");
 const db = require("../models");
 
 module.exports = function (app) {
+
+    // Scrape Route
     app.get("/scrape", function (req, res) {
-        axios.get("https://www.https://www.apnews.com/")
+        axios.get("https://www.https://www.apnews.com/apf-topnews")
             .then(function (response) {
                 const $ = cheerio.load(response.data)
 
-                let results = {};
-                
-                let headline = $(element).find("a").find("h1").text();
-                let link = "https://apnews.com" + $(element).find("a").attr("href");
-                let image = $(element).find
+                $("div.FeedCard").each(function (i, element) {
 
-            })
+                    let headline = $(element).find("a").find("h1").text();
+                    let link = "https://apnews.com" + $(element).find("a").attr("href");
+                    let summary = $(element).find("a").find("div.content").find("p").text();
 
-        console.log(results)
-    })
-}
+                    let results = {
+                        headline: headline,
+                        link: link,
+                        summary: summary
+                    };
+
+                    db.Article.create(results)
+                    .then(function(dbArticle) {
+                        console.log(dbArticle)
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                    });
+                });
+
+                res.send("Srape Completed Successfully")
+            });
+
+    });
+
+    // Route for getting all Articles in db
+    app.get("/articles", function (req, res) {
+        db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    });
+
+    // Route for grabbing specific Article by id & populate with its associated Note
+    app.get("/articles/:id", function(req,res) {
+        db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    });
+
+    // Route for saving/updating an Article's associated Note
+    app.post("/articles/:id", function(req, res) {
+        db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, {new: true });
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    });
+};
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,8 +7,4 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/html/index.html"));
     });
 
-    app.get("/saved", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/html/saved.html"));
-    });
-
 };
